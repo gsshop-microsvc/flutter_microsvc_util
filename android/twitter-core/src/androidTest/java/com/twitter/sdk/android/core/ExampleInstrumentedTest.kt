ba021ee3185bae6332e54746c5159cbd4049029f
문석희/전시시DX팀 <zazachucky@gsretail.com>
2025-03-24 09:51:36 +0900

    twitter sdk 일몰에 따른 내부 패키지화

--- android/twitter-core/src/androidTest/java/com/twitter/sdk/android/core/ExampleInstrumentedTest.kt
+++ android/twitter-core/src/androidTest/java/com/twitter/sdk/android/core/ExampleInstrumentedTest.kt
@@ -1,6 +1,6 @@
 package com.twitter.sdk.android.core
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test

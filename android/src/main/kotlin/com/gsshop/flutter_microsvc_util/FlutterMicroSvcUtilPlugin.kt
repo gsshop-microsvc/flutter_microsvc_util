e088f82d05c8fd9f9f7494a43423445e96441f7c
문석희/전시DX팀 <zazachucky@gsretail.com>
2025-04-25 14:34:09 +0900

    kotlin code 수정

--- android/src/main/kotlin/com/gsshop/flutter_microsvc_util/FlutterMicroSvcUtilPlugin.kt
+++ android/src/main/kotlin/com/gsshop/flutter_microsvc_util/FlutterMicroSvcUtilPlugin.kt
@@ -1,35 +1,30 @@
 package com.gsshop.flutter_microsvc_util
 
+// import com.twitter.sdk.android.tweetcomposer.TweetComposer
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.*
-import android.content.Context
 import android.content.pm.PackageManager
-import android.media.FaceDetector
 import android.net.Uri
-import androidx.annotation.NonNull
 import android.os.Bundle
-
-import io.flutter.Log
+import androidx.annotation.NonNull
+import com.facebook.CallbackManager
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.share.model.ShareLinkContent
+import com.facebook.share.widget.ShareDialog
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-
+import java.io.File
 import java.net.MalformedURLException
 import java.net.URL
 import java.net.URLEncoder
-
-// import com.twitter.sdk.android.tweetcomposer.TweetComposer
-import com.facebook.FacebookSdk
-import com.facebook.CallbackManager
-import com.facebook.share.model.ShareLinkContent
-import com.facebook.share.widget.ShareDialog
-import com.facebook.appevents.AppEventsLogger
 import java.util.*
 
 
@@ -148,7 +143,39 @@ class FlutterMicroSvcUtilPlugin: FlutterPlugin, MethodCallHandler, ActivityAware
    * @param result Result
    */
 
-   private fun shareToFacebook(url: String?, quote: String?, result: Result) {
+  @SuppressLint("NewApi")
+  private fun shareToFacebook(url: String?, quote: String?, result: Result) {
+    try {
+      val intent = Intent(Intent.ACTION_SEND)
+
+        // If no image is provided, share the message as text
+        intent.setType("text/plain")
+
+
+      // Add the message if provided
+      var str = "";
+      if (quote != null && !quote.isEmpty()) {
+        str = "${str}quote"
+//        intent.putExtra(Intent.EXTRA_TEXT, quote)
+      }
+      if (url != null && !url.isEmpty()) {
+        str = "${str}\nurl"
+      }
+
+      intent.putExtra(Intent.EXTRA_TEXT, str)
+      // Set the package to Facebook
+      intent.setPackage("com.facebook.katana")
+
+      activity!!.startActivity(intent)
+      result.success("success")
+    } catch (e: ActivityNotFoundException) {
+      result.error("FACEBOOK_NOT_INSTALLED", "Facebook is not installed on this device", null)
+    } catch (e: java.lang.Exception) {
+      result.error("ERROR", e.toString(), null)
+    }
+  }
+
+   private fun shareToFacebook222(url: String?, quote: String?, result: Result) {
     FacebookSdk.sdkInitialize(activityContext!!)
     if (url.isNullOrEmpty()) {
         result.error("INVALID_URL", "URL이 비어 있습니다.", null)
